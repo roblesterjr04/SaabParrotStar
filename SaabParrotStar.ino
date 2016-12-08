86a762359ac76d5538183e6e4bf77e1c8285fa3d
Robert Lester <robertlesterjr@mac.com>
2016-12-08 18:20:05 -0500

    Code cleanup and values adjusted. Code is good for prototype board.

--- SaabParrotStar.ino
+++ SaabParrotStar.ino
@@ -23,20 +23,20 @@ int executed = 0;
 // Below ranges based on a 1K resistor pulling down the button value line.
 
 // Button 1 analog range
-int btn1low = 220;
-int btn1high = 235;
+int btn1low = 101;
+int btn1high = 299;
 
 // Button 2 analog range
-int btn2low = 55;
-int btn2high = 65;
+int btn2low = 1;
+int btn2high = 100;
 
 // Button 3 analog range
-int btn3low = 370;
-int btn3high = 380;
+int btn3low = 300;
+int btn3high = 400;
 
 // Button Press Debounce/Longpress settings
 int longpress = 1000; //How long should a long press be
-int presstimeout = 75; //Set the firsttime value after this long of a press.
+int presstimeout = 200; //Set the firsttime value after this long of a press.
 
 // Comms - don't change these
 int conRes = 128;
@@ -60,9 +60,6 @@ int resetConfirm = 0;
 int inCall = 0;
 
 void setup() { // Initial Boot Sequence
-  
-	Serial.begin(9600);
-	Serial.println("Ready.");
 	pinMode(greenPin, OUTPUT);
 	pinMode(redPin, OUTPUT);
 	pinMode(ledPin, OUTPUT);
@@ -71,12 +68,11 @@ void setup() { // Initial Boot Sequence
 	box.begin(9600);
 	digitalWrite(auxPin1, EEPROM.read(auxPinMem1));
 	digitalWrite(auxPin2, EEPROM.read(auxPinMem2));
-	
+	blinkLed(ledPin, 5);
 }
 void(* resetFunc) (void) = 0;
 
 void loop() { // 
-  
 	readAnalogController(); //Listen for analog button commands.
 	
 	// Tell brain Box that we are waiting.
@@ -86,10 +82,7 @@ void loop() { //
 		delay(250);
 		int bl = digitalRead(greenPin);
 		digitalWrite(greenPin, !bl);
-		//int dp = digitalRead(ledPin);
-		//digitalWrite(ledPin, !dp);
 	}
-  
 }
 
 void serialEvent1() { // Receive commands from the brain box
@@ -105,10 +98,27 @@ void serialEvent1() { // Receive commands from the brain box
 	
 	if (v == 34) { // At rest
 		digitalWrite(greenPin, LOW);
+		digitalWrite(redPin, LOW);
+		inCall = 0;
+		menu = 0;
 	}
 	if (v == 17) { // Phone is engaged..?
 		digitalWrite(greenPin, HIGH);
 		digitalWrite(redPin, LOW);
+		inCall = 1;
+	}
+	if (v == 32) {
+		digitalWrite(greenPin, LOW);
+		digitalWrite(redPin, HIGH);
+		menu = 1;
+	}
+	if (v == 2) {
+		digitalWrite(greenPin, HIGH);
+		digitalWrite(redPin, LOW);
+		menu = 1;
+	}
+	if (v == 119) {
+		blinkLed(greenPin, 3);
 	}
 	
 	// End specific commands
@@ -135,51 +145,11 @@ void serialEvent1() { // Receive commands from the brain box
 		conRes = 131;
 	}
 	
-	// Print non-handshake/status messages to the console.
-	if (v != 4 && v != 160 && v != 1) {
-		Serial.print("Box: ");
-		Serial.println(v, DEC);
-	}
-	
 	lastComm = v; // Set our last command
 }
 
-/*void serialEvent() {
-	
-	// The below commands are strictly for sending 
-	// commands to box via the command line.
-
-	int c = Serial.read();
-
-	if (c == 56 || c == 55 || c == 52) {
-		box.write(160);
-		box.write(130);
-		if (c == 56) box.write(8);
-		if (c == 55) box.write(248);
-		if (c == 52) {
-			box.write(4);
-			delay(250);
-			box.write(160);
-			box.write(130);
-			box.write(0);
-		}
-	} else {
-		box.write(160);
-		box.write(130);
-		box.write(c);
-		delay(250);
-		box.write(160);
-		box.write(130);
-		box.write(0);
-	}
-	Serial.print("Rec: ");
-	Serial.println(c);
-}*/
-
 void readAnalogController() {
 	current = analogRead(buttonPin);
-	//longpress = analogRead(1); // Use if there's a pot to set longpress time.
-	//if (current > 0) Serial.println(current); // Use this line to determine actual analog values initially.
 	
 	if (previous > 0 && current == 0) {
 		executed = 0;
@@ -208,7 +178,7 @@ void readAnalogController() {
 	
 	}
 	
-	if (millis_held > longpress * 2) {
+	if (millis_held > longpress * 10) {
 		
 		int btn = decodeButton(current);
 		executeButton(btn, 2);
@@ -219,7 +189,7 @@ void readAnalogController() {
 }
 
 int decodeButton(int value) {
-
+	
 	if (value > btn1low && value < btn1high) return 1;
 	else if (value > btn2low && value < btn2high) return 2;
 	else if (value > btn3low && value < btn3high) return 3;
@@ -270,6 +240,7 @@ void longPressButton(int button) {
 			break;
 		case 2:
 			if (!menu && !inCall) auxEnable1();
+      if (inCall) menuSelect();
 			break;
 		case 3:
 			if (menu || inCall) endButton();
@@ -296,9 +267,9 @@ void blinkLed(int led, int times) {
 	int counter = 0;
 	while (counter < times) {
 		digitalWrite(led, HIGH);
-		delay(50);
+		delay(100);
 		digitalWrite(led, LOW);
-		delay(50);
+		delay(100);
 		counter++;
 	}
 }
@@ -324,21 +295,15 @@ void callButton() {
 void redial() {
 	command();
 	box.write(2);
-	delay(850);
-	releaseButton();
 }
 
 void endButton() {
-	digitalWrite(redPin, LOW);
-	menu = 0;
 	command();
 	box.write(1);
 	releaseButton();
 }
 
 void menuSelect() {
-	digitalWrite(redPin, HIGH);
-	menu = 1;
 	command();
 	box.write(4);
 	releaseButton();
@@ -361,7 +326,6 @@ void reset() {
 	command();
 	box.write(3);
 	delay(2000);
-	//releaseButton();
 	digitalWrite(redPin, LOW);
 	EEPROM.update(auxPinMem1, LOW);
 	EEPROM.update(auxPinMem2, LOW);
